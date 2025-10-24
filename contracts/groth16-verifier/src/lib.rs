fc874635af947c5b05608fcac27f7d90b73684f5
Matteo Lisotto <matteo.lisotto@gmail.com>
2025-10-24 10:43:20 +0200

    Enable bn254 functions at smart contract level (#3)
    * feat(crypto): add `bn254` curve
    
    This types are temporary until Stellar (and `soroban-sdk`) introduces
    precompiles.
    
    * feat(bn254): add `Fr` local implementation
    
    * fix(bn254): change From impls to take references instead of owned values
    
    * test(bn254): add unit tests
    
    * fix(verifier): remove `groth16` module from `lib.rs`
    
    * chore: rename `risc0-verifier` to `groth16-verifier`

--- contracts/groth16-verifier/src/lib.rs
+++ contracts/groth16-verifier/src/lib.rs
@@ -1,10 +1,15 @@
 #![no_std]
+
+// Use Soroban's allocator for heap allocations
+extern crate alloc;
+
 use soroban_sdk::{contract, contractimpl};
+
+mod crypto;
+mod test;
 
 #[contract]
 pub struct RiscZeroVerifier;
 
 #[contractimpl]
 impl RiscZeroVerifier {}
-
-mod test;
